b3ac5efb472a7eab32c8f2afe0dcafd94e3fb8f8
drdunc <drdunc@gmail.com>
2020-07-08 18:42:44 +0100

    nano33 temp minimal
    minimal sketch for sending tep / hum data every second via BLE

--- nano33ble_temp_hum/nano33ble_temp_hum.ino
+++ nano33ble_temp_hum/nano33ble_temp_hum.ino
@@ -1,16 +1,30 @@
-
+/*  Project PAPR July 2020
+ *  
+ *  Duncan Wilson https://github.com/djdunc/papr
+ *  
+ *  Code based on examples ArduinoBLE, Arduino_HTS221 and ClosedCube_HDC1080 libaries
+ *  
+ *  Hardware: Arduino Nano 33 Sense and a Closed Cube HDC1080 temp / hum I2C sensor
+ *  
+ *  HDC1080 connected via A4 and A5
+ *  
+ *  Sketch is used to create a peripheral that sends temp and humidity readings over BLE
+ *  
+ */
+   
 
 #include <ArduinoBLE.h>
 #include <Arduino_HTS221.h>
 #include <ClosedCube_HDC1080.h>
 
+// temperature and humidity I2C sensor https://github.com/closedcube/ClosedCube_HDC1080_Arduino
 ClosedCube_HDC1080 hdc1080;
 
- // BLE Environmental Sensing Service
+ // BLE Environmental Sensing Service https://www.bluetooth.com/specifications/gatt/services/ 
 BLEService HDC1080Service("181A");
 
-// BLE Temp in C Characteristic - see https://www.bluetooth.com/specifications/gatt/characteristics/ fr UUID's
-// standard 16-bit characteristic UUID https://www.bluetooth.com/xml-viewer/?src=https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.temperature.xml
+// BLE Temp in C Characteristic - see https://www.bluetooth.com/specifications/gatt/characteristics/ for UUID's
+// standard 16-bit characteristic UUID 
 BLEFloatCharacteristic temperatureChar("2A6E", BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
 BLEFloatCharacteristic humidityChar("2A6F", BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
 
@@ -19,33 +33,37 @@ float humidity;
 long previousMillis = 0;  // last time the battery level was checked, in ms
 
 void setup() {
-  delay(5000); // seen some issues with serial not connecting immediately on nano 33 ble hence giving it time to connect 
-  Serial.begin(9600);    // initialize serial communication
-  Serial.println("Setting up...");
+  delay(5000);            // seen some issues with serial not connecting immediately on nano 33 ble hence giving it time to connect 
+  Serial.begin(9600);     // initialize serial communication
   pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
-
+  digitalWrite(LED_PWR, LOW); // turning off the power led to save energy
+  
   // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
-
-    while (1);
+    while (1){
+      blinky(1);    
+    }
   }
 
   // initialise on board sensors
   if (!HTS.begin()) {
     Serial.println("Failed to initialize humidity temperature sensor!");
-    while (1);
+    while (1){
+      blinky(1);
+    }
   }
 
   // Default settings for HDC1080 sensors: - Heater off - 14 bit Temperature and Humidity Measurement Resolutions
   hdc1080.begin(0x40);
 
+/*
   Serial.print("Manufacturer ID=0x");
   Serial.println(hdc1080.readManufacturerId(), HEX); // 0x5449 ID of Texas Instruments
   Serial.print("Device ID=0x");
   Serial.println(hdc1080.readDeviceId(), HEX); // 0x1050 ID of the device 
   printSerialNumber();
-
+*/
 
   /* Set a local name for the BLE device
      This name will appear in advertising packets and can be used by remote devices to identify this BLE device
@@ -72,39 +90,40 @@ void loop() {
 
   // if a central is connected to the peripheral:
   if (central) {
-    Serial.print("Connected to central: "); // print the central's BT address:
-    Serial.println(central.address());      // turn on the LED to indicate the connection:
-    digitalWrite(LED_BUILTIN, HIGH);
+    blinky(5);
+//    Serial.print("Connected to central: "); // print the central's BT address:
+//    Serial.println(central.address());      // turn on the LED to indicate the connection:
 
     // check the status while the central is connected:
     while (central.connected()) {
-      long currentMillis = millis();
-      // if 1000ms have passed, check the sensor:
-      if (currentMillis - previousMillis >= 1000) {
-        previousMillis = currentMillis;
-        updateReadings();
-      }
+      updateReadings();
+      delay(1000);                   // take a reading every second while something is listening
     }
-    // when the central disconnects, turn off the LED:
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.print("Disconnected from central: ");
-    Serial.println(central.address());
+      // when the central disconnects
+      blinky(3);
+//    Serial.print("Disconnected from central: ");
+//    Serial.println(central.address());
   }
+  delay(1000);
 }
 
 void updateReadings() {
   
+  // take readings from the HDC1080
   temperature = hdc1080.readTemperature();
   humidity = hdc1080.readHumidity();
+  temperatureChar.writeValue(temperature);
+  humidityChar.writeValue(humidity);
+/*
   Serial.print("T=");
   Serial.print(temperature);
   Serial.print("C, RH=");
   Serial.print(humidity);
   Serial.println("%");
-  temperatureChar.writeValue(temperature);
-  humidityChar.writeValue(humidity);
-
+*/
   
+/* some code to check the readings being sent over BLE
+
     float tempVal[4]; // hold the characteristic's bytes
     temperatureChar.readValue(tempVal, 4);
     Serial.print(tempVal[0]);
@@ -114,8 +133,10 @@ void updateReadings() {
     humidityChar.readValue(humVal, 4);
     Serial.print(humVal[0]);
     Serial.println(" % ");
+*/
 
-
+/*  code below reads sensor values from onboard sensors - not being used on live system
+    
   // read all the sensor values
   temperature = HTS.readTemperature();
   humidity    = HTS.readHumidity();
@@ -129,12 +150,25 @@ void updateReadings() {
 
   // print an empty line
   Serial.println();
+ */
+
 }
 
+/*
 void printSerialNumber() {
   Serial.print("Device Serial Number=");
   HDC1080_SerialNumber sernum = hdc1080.readSerialNumber();
   char format[12];
   sprintf(format, "%02X-%04X-%04X", sernum.serialFirst, sernum.serialMid, sernum.serialLast);
   Serial.println(format);
+}
+*/
+
+void blinky(int n){
+  for(int i = 0; i < n; i++){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);      
+  }
 }
